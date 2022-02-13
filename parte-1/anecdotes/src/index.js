5cb0a34cf25c6806f161e5dedab49f4d2788d841
Ariel Alexis Lorda Barrabasqui <Ariel.Lorda@inworx.com>
2022-02-13 08:18:39 -0300

    parte-1

--- parte-1/anecdotes/src/index.js
+++ parte-1/anecdotes/src/index.js
@@ -1,27 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  <h1>{props.course}</h1>
-}
-
-
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+const Button = (props) => {
   return (
     <div>
-      <Header course={course} />
-      <Content />
-      <Total />
+      <button onClick={props.handlerClick}>{props.text}</button>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [point, setPoint] = useState([0, 0, 0, 0, 0, 0])
+  const [ubiAnec, setUbiAnec] = useState(0)
+  const [maxVal, setMaxVal] = useState(0)
+
+  const newSelected = () => {
+    setSelected(Math.floor(Math.random() * (6 - 0)) + 0)
+    maxVotes();
+  };
+
+  const newVote = () => {
+    const copy = [...point]
+    copy[selected] += 1
+    setPoint(copy)
+    maxVotes();
+  }
+
+  const maxVotes = () => {
+    for (let index = 0; index < point.length; index++) {
+      const elemen = point[index];
+      if (maxVal < elemen) {
+        setUbiAnec(index)
+        setMaxVal(elemen)
+      }
+    }
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {props.anecdotes[selected]}
+      <p>Has {point[selected]}</p>
+      <div>
+        <Button handlerClick={() => newVote()} text='Vote' />
+        <Button handlerClick={() => newSelected()} text='New Anecdote' />
+      </div>
+      <h1>Anecdote with most votes</h1>
+      {props.anecdotes[ubiAnec]}
+      <p>Has {point[ubiAnec]}</p>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
